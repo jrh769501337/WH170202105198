194849fe417051e6be0a226537e4b2112d6adc67
Administrator <Administrator@MD502-79>
2019-05-22 09:21:10 +0800

    4

---
+++ src/com/bl/student.java
@@ -0,0 +1,10 @@
+package com.bl;
+
+public class student {
+	public String name;
+	   public String address;
+	   
+	   student(){
+		   
+	   }
+}
---
+++ src/com/bl/teacher.java
@@ -0,0 +1,11 @@
+package com.bl;
+
+public class teacher {
+	public String name;
+	   public String address;
+	   
+	   teacher(){
+		   
+	   }
+
+}
